c5e02aa0800f808bec8c5637499948f29d1a5acd
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2016-05-28 13:50:33 +0200

    Update carSetup.ino

--- Arduino/carSetup/carSetup.ino
+++ Arduino/carSetup/carSetup.ino
@@ -4,16 +4,16 @@ Car car;
 SR04 ultrasonicSensorFront;
 const int TRIGGER_PIN = 6; //D6
 const int ECHO_PIN = 5; //D5
-int distanceFront;
+SR04 ultrasonicSensorBack;
+const int TRIGGER_PIN_BACK = 10; //D10
+const int ECHO_PIN_BACK = 9; //D9
+int distanceFront, distanceBack;
 Odometer encoderLeft, encoderRight;
 Gyroscope gyro;
-int frontSpeed = 40; //70% of the full speed forward
-int backSpeed = -40; //70% of the full speed backward
+int frontSpeed = 70; //70% of the full speed forward
+int backSpeed = -70; //70% of the full speed backward
 int lDegrees = -75; //degrees to turn left
 int rDegrees = 75; //degrees to turn right
-boolean side = false;
-
-int buzzer = 8;
 
 
 void setup() {
@@ -24,33 +24,14 @@ void setup() {
   gyro.begin();
   Serial.begin(9600);
   car.begin(encoderLeft, encoderRight, gyro); //initialize the car using the encoders and the gyro
-  ultrasonicSensorFront.attach(TRIGGER_PIN, ECHO_PIN);
+  ultrasonicSensorFront.attach(6, 5);
+  ultrasonicSensorBack.attach(10, 9);
   distanceFront = 0;
-  pinMode(buzzer, 0);
+  distanceBack = 0;
 }
 
 void loop() {
-  distanceFront = ultrasonicSensorFront.getDistance();
   handleInput();
-  if ( frontIsClear() == false  && side == true){  
-        if (distanceFront > 15 && distanceFront == 0){
-           car.setSpeed(frontSpeed);      
-        }
-        else{
-           pinMode(buzzer, OUTPUT);
-           car.setSpeed(frontSpeed - 20);
-           delay(500);
-           pinMode(buzzer, 0);
-           car.setSpeed(frontSpeed - 40);          
-           digitalWrite(buzzer, LOW);
-           delay(100);
-           pinMode(buzzer, OUTPUT);
-           delay(500);
-           car.setSpeed(0);
-           pinMode(buzzer, 0);
-           side = false;    
-        }
-    }; 
 }
 
 void handleInput() { //handle serial input if there is any
@@ -59,28 +40,22 @@ void handleInput() { //handle serial input if there is any
     char input = Serial3.read(); //read everything that has been received so far and log down the last entry
     switch (input) {
       case 'l': //rotate counter-clockwise going forward
-        Serial.print('l');
         car.setSpeed(frontSpeed);
-        car.setAngle(lDegrees);        break;
+        car.setAngle(lDegrees);
+        break;
       case 'r': //turn clock-wise
-         Serial.print('r');
-         car.setSpeed(frontSpeed);
-         car.setAngle(rDegrees);
-         break;
-      case 'f': //go ahead
-        Serial.print('f');
-        side = true;
-        car.setAngle(0); 
         car.setSpeed(frontSpeed);
+        car.setAngle(rDegrees);
+        break;
+      case 'f': //go ahead
+        car.setSpeed(frontSpeed);
+        car.setAngle(0);
         break;
       case 'b': //go back
-        Serial.print('b');
-        side = false;
         car.setSpeed(backSpeed);
         car.setAngle(0);
         break;
       case 's': //if you receive something that you don't know, just stop
-        Serial.print('s');
         car.setSpeed(0);
         car.setAngle(0);
     }
@@ -89,14 +64,22 @@ void handleInput() { //handle serial input if there is any
 
   boolean frontIsClear(){
    distanceFront = ultrasonicSensorFront.getDistance();
-    if (distanceFront > 10){
-      Serial.print(distanceFront); 
+    if (distanceFront > 10)
+      Serial.println(distanceFront); 
       return true;
-      }
-     if (distanceFront == 0){
+    if (distanceFront == 0)
       return true;
-     }
-     else{
-        return false;
+
+    Serial.print(distanceFront);
+    return false;
   }
-  }
+
+  boolean backIsClear(){
+   distanceBack = ultrasonicSensorBack.getDistance();
+   if (distanceBack > 10)
+      return true;
+   if (distanceBack == 0)
+      return true;
+
+   return false;
+}
